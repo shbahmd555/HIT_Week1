5e25b3e2ed2220652d2089f7182b6711bc1062cc
shoaib <shoaib.ahmed@girmiti.com>
2021-06-07 21:39:29 +0530

    First Commit

---
+++ Basic/src/com/sas/Basic.java
@@ -0,0 +1,8 @@
+package com.sas;
+
+public class Basic
+{
+ public static void main(String[] args) {
+	System.out.println("Welcome to Eclipse");
+}
+}
---
+++ Basic/src/com/sas/checkEvenOdd.java
@@ -0,0 +1,15 @@
+package com.sas;
+
+public class checkEvenOdd {
+	public static void main(String args[])
+	{
+	int num=20;
+	/* If number is divisible by 2 then it's an even number
+	* else odd number*/
+	if ( num % 2 == 0 )
+	System.out.println("Entered number is even");
+	else
+	System.out.println("Entered number is odd");
+
+}
+}
